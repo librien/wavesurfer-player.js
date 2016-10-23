0d3d82f8011e6bd599296e2f94c3e675e6693905
Kevin <kevin.matthew.obrien@gmail.com>
2016-10-23 00:20:10 -0400

    Update wavesurfer-player.js

--- docs/js/wavesurfer-player.js
+++ docs/js/wavesurfer-player.js
@@ -4,6 +4,9 @@ wavesurfer-player.js
 
 $('.song-menu').dropdown(); //initiate bootstrap dropdown
 // Replace .dropdown with .dropup if at the bottom of the scrollable area in .playlist div
+$('.song-menu').click(function(){
+ console.log('clicked');
+});
 $(".dropdown").click(function(){
  console.log('running');
  var dropdownClassCheck = $(this).hasClass('dropdown');
