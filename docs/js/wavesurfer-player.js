73a189b31488fdfefa1b163100a4c361e67022f6
Kevin <kevin.matthew.obrien@gmail.com>
2016-10-22 21:16:30 -0400

    Update wavesurfer-player.js

--- docs/js/wavesurfer-player.js
+++ docs/js/wavesurfer-player.js
@@ -14,21 +14,32 @@ else {
 }
 $('.song-menu').dropdown(); //initiate bootstrap dropdown
 // Replace .dropdown with .dropup if at the bottom of the scrollable area in .playlist div
-$(document).on("shown.bs.dropdown", ".dropdown", function () {
-    // calculate the required sizes, spaces
-    var $ul = $(this).children(".dropdown-menu");
-    var $button = $(this).children(".song-menu");
-    var ulOffset = $ul.offset();
-    // how much space would be left on the top if the dropdown opened that direction
-    var spaceUp = ((ulOffset.top) - $button.height() - $ul.height()) - $('#playlist').scrollTop();
-    // how much space is left at the bottom
-    var spaceDown = $('#playlist').scrollTop() + $('#playlist').height() - ((ulOffset.top + 10) + $ul.height());
-    // switch to dropup only if there is no space at the bottom AND there is space at the top, or there isn't either but it would be still better fit
-    if (spaceDown < 0 && (spaceUp >= 0 || spaceUp > spaceDown))
-      $(this).addClass("dropup");
-}).on("hidden.bs.dropdown", ".dropdown", function() {
-    // always reset after close
-    $(this).removeClass("dropup");
+$('.dropdown').each(function(index,element){
+    var $dropDown = $(element),
+        $dropDownMenu = $dropDown.find('.dropdown-menu'),
+        dropDownTop = $dropDown.offset().top,
+        visibleHeight = $dropDown.height(),
+        hiddenHeight = $dropDownMenu.height(),
+        ddTop = dropDownTop - hiddenHeight,
+        ddBottom = dropDownTop + visibleHeight + hiddenHeight;
+
+  // LOOP through all parents
+  $dropDown.parents().each(function(ix,el){
+    var $el = $(el);
+
+    // CHECK if any of them have overflow property set
+    if( $el.css('overflow') !== 'visible' ){
+        var limitTop = $el.offset().top,
+            limitBottom = limitTop + $el.height();
+
+      // CHECK if parent is better fit when dropped upside
+      if( limitBottom < ddBottom && ( ddTop - limitTop ) > ( limitBottom - ddBottom ) )
+        $dropDown.removeClass('dropdown').addClass('dropup');
+
+      // BREAK LOOP
+      return false;
+    }
+  });
 });
 
 // Create a WaveSurfer instance
